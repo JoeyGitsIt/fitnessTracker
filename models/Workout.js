0aa383e958797ab8f4e57932145f8ceb91c175db
JoeyGitsIt <josephperri3@gmail.com>
2021-11-10 21:27:21 -0500

    added content to server.js, started making routes, and created the model

---
+++ models/Workout.js
@@ -0,0 +1,44 @@
+// took this bit from mini project at the moment
+// 11/5 (friday) paul goes over the project
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        required: "Enter workout name",
+      },
+      type: {
+        type: String,
+        required: "Enter a workout type",
+      },
+      duration: {
+        type: Number,
+        required: "Enter a number",
+      },
+      distance: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
